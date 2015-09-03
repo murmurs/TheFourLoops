e2c38260c6760e64172473bbb9e9679fd17e91af
Sin Wai Lam <sinwailam193@hotmail.com>
2015-09-03 11:10:20 -0700

    install socket.io and server ran smoothly

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 var express = require('express');
-var server = express();
+var expressInvoked = express();
+var server = require('http').Server(expressInvoked);
 var port = process.env.Port || 3000;
 var path = require('path');
+var io = require('socket.io')(server);
 
-server.use(express.static(path.join(__dirname, '../public')));
+io.on('connection', function(socket){
 
-server.listen(port, function(){
+  socket.on('event', function(data){});
+
+  socket.on('disconnect', function(){
+    console.log("we're disconnected");
+  })
+
+})
+
+expressInvoked.use(express.static(path.join(__dirname, '../public')));
+
+expressInvoked.listen(port, function(){
   console.log("it's working")
 });
