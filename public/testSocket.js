1ff89c4f0f94e72b099891764892c1fd8d46ff83
codeCommode <james.harlan@gmail.com>
2015-09-03 20:41:33 -0700

    sockets working -- manipulating other screen

--- public/testSocket.js
+++ public/testSocket.js
@@ -1,7 +1,26 @@
 (function(){
+
   var socket = io.connect('http://localhost:3000');
-  var text = document.getElementById('text');
-  text.addEventListener('input', function(event){
-    socket.emit('typing', {text:this.value});
+  var userId;
+  socket.on('userId', function(data){
+    userId = data.userId
+    console.log(userId);
+  });
+  var input = document.getElementById('input');
+  var opponent = document.getElementById('opponent');
+  socket.on('typing', function(data){
+    // console.log('user typed');
+    if(data.userId !== userId){
+      opponent.innerHTML = data.code;
+    }
+  });
+
+  input.addEventListener('input', function(event){
+    // console.log(this.value);
+    socket.emit('typing', {
+      code:this.value,
+      userId:userId
+    });
   })
+
 })();
