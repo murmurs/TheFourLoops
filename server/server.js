9ecf5138ba63eb02d33fa9af5246a84e64ca577f
Sin Wai Lam <sinwailam193@hotmail.com>
2015-09-04 13:27:17 -0700

    added firebase in the server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var Firebase = require("firebase");
 
 var port = process.env.PORT || 3000;
 
@@ -20,4 +21,18 @@ io.on('connection', function (socket) {
   socket.on('typing', function (data) { //emit typing regardless of user id. do the check on the client side.
     io.sockets.emit('typing', data); //data has 2 properties, userId and code.
   });
+});
+
+var dataRef = new Firebase("https://popping-heat-272.firebaseio.com/");
+
+dataRef.push({
+  challenges: {
+    sum: {
+      Question: "Writ a sum function that sums up all of its parameters.",
+      Answers: [
+        {"input1": [1,2,3], "output1": 6},
+        {"input1": [10,20,3], "output1": 33}
+      ]
+    }
+  }
 });
