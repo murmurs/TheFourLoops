0191578ab0041926c526c47fb3d96d80f3ccabd2
Dylan Wright <dylansamuelwright@gmail.com>
2015-09-07 12:32:03 -0700

    deleted testSocket

--- public/testSocket.js
+++
@@ -1,7 +0,0 @@
-(function test(){
-  var text = document.getElementById('text');
-  text.addEventListener('input', function(event){
-    socket.emit('typing', {test: 123});
-  })
-  var socket = io.connect('http://localhost:3000');
-})();
