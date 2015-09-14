bf31b3990c5d48b7bd74c7fbd6420fbd56ecc0b2
Dylan Wright <dylansamuelwright@gmail.com>
2015-09-13 22:40:56 -0700

    added testing

--- public/client/evalWorker.js
+++ public/client/evalWorker.js
@@ -1,43 +1,13 @@
+importScripts('appUtilities.js');
+
 onmessage = function(e) {
 
   console.log("start of worker");
 
-  var evaluateTests = function(challengeFunction, inputs, answers) {
-    var responseArr = [];
-    console.log("tests started");
-    for (var i=0; i<inputs.length; i++){
-      var functionResponse = challengeFunction(inputs[i]);
-      console.log("functionResponse", functionResponse);
-      var testResponse = {
-        input: inputs[i],
-        answer: answers[i],
-        functionResponse: functionResponse
-      };
-      console.log("testing input");
-      console.log(testResponse);
-      if (functionResponse === answers[i]){ //if the function processes the input to the expected, and answer is correct.
-        testResponse.valid = true;
-        responseArr.push(testResponse);
-      }
-      else {
-        if (functionResponse === undefined) {
-          testResponse.functionResponse = "undefined";
-        }
-        testResponse.valid = false;
-        console.log("test failed!");
-        responseArr.push(testResponse);
-        break; //break out of the for loop so the testing does not continue.
-      }
-
-    }
-    
-    console.log("testingArr complete", responseArr[0]);
-    return responseArr; //all tests are valid, so return.
-  };
-
   var codeProcess = function(functionName, inputs, answers){ //determine the text response.
     //console.log(functionName, typeof functionName);
     var challengeFunction = eval(functionName);
+
     console.log("top of codeprocess");
     console.log(challengeFunction);
     
@@ -46,7 +16,7 @@ onmessage = function(e) {
       var codeResponse = {
         valid: true,
         error: "The submitted code has no syntax or timeout errors",
-        tests: evaluateTests(challengeFunction, inputs, answers)
+        tests: testingUtils.evaluateTests(challengeFunction, inputs, answers)
       }
     }
     else {
@@ -77,12 +47,3 @@ onmessage = function(e) {
   console.log("end of code response");
   postMessage(codeResponse);
 }
-
-//sample function that i've been using for sum.
-// var sum = function(arr){
-//   var total = 0;
-//   for (var i=0; i<arr.length; i++){
-//     total += arr[i];
-//   }
-//   return total;
-// }
