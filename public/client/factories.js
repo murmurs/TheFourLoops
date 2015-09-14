badef50fec73c5626479310eb1e117f8bfcbc136
Sin Wai Lam <sinwailam193@hotmail.com>
2015-09-14 11:23:46 -0700

    clean up factories.js to make it the correct syntax, and deleted a tag in index.html

--- public/client/factories.js
+++ public/client/factories.js
@@ -2,32 +2,38 @@ angular.module('coderace.factories', [])
 
 .factory('socket', function ($rootScope) {
   var socket = io.connect();
-  return {
-    on: function (eventName, callback) {
-      socket.on(eventName, function () {  
-        var args = arguments;
-        $rootScope.$apply(function () {
-          callback.apply(socket, args);
-        });
+
+  var factory = {};
+
+  factory.on = function (eventName, callback) {
+    socket.on(eventName, function () {  
+      var args = arguments;
+      $rootScope.$apply(function () {
+        callback.apply(socket, args);
       });
-    },
-    emit: function (eventName, data, callback) {
-      socket.emit(eventName, data, function () {
-        var args = arguments;
-        $rootScope.$apply(function () {
-          if (callback) {
-            callback.apply(socket, args);
-          }
-        });
-      });
-    },
-    disconnect: function(){
-      socket.disconnect();
-    },
-    connect: function(){
-      socket.connect();
-    }
+    });
   };
+
+  factory.emit = function (eventName, data, callback) {
+    socket.emit(eventName, data, function () {
+      var args = arguments;
+      $rootScope.$apply(function () {
+        if (callback) {
+          callback.apply(socket, args);
+        }
+      });
+    });
+  };
+
+  factory.disconnect = function(){
+    socket.disconnect();
+  };
+
+  factory.connect = function(){
+    socket.connect();
+  };
+
+  return factory;
 })
 
 .factory('Race', function ($rootScope) {
@@ -36,23 +42,6 @@ angular.module('coderace.factories', [])
   factory.username;
 
   factory.dataRef = new Firebase("https://popping-heat-272.firebaseio.com/");
-
-  // factory.dataRef.child("Challenges").set([
-  //   {
-  //     functionName: "sum",
-  //     question: "Write a sum function that sums up all of its parameters",
-  //     inputs: [[1,2,3], [10, 20, 3]],
-  //     answers: [6, 33],
-  //     startingCode: "var sum = function() {}"
-  //   },
-  //   {
-  //     functionName: "reverse",
-  //     question: "Write a reverse function that reverses the string",
-  //     inputs: ['run', 'face'],
-  //     answers: ['nur', 'ecaf'],
-  //     startingCode: "var reverse = function() {}"
-  //   }
-  // ]);
 
   /*  called by slave user  */
   factory.setProblem = function(data){
