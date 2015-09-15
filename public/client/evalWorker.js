c3c255d0712d4427a902b5c265101a78fe66288d
Dylan Wright <dylansamuelwright@gmail.com>
2015-09-14 17:55:42 -0700

    fixed code evaluation bug

--- public/client/evalWorker.js
+++ public/client/evalWorker.js
@@ -2,14 +2,8 @@ importScripts('appUtilities.js');
 
 onmessage = function(e) {
 
-  console.log("start of worker");
-
   var codeProcess = function(functionName, inputs, answers){ //determine the text response.
-    //console.log(functionName, typeof functionName);
     var challengeFunction = eval(functionName);
-
-    console.log("top of codeprocess");
-    console.log(challengeFunction);
     
     if (typeof challengeFunction === "function"){
       console.log("function is correctly a function");
@@ -27,7 +21,6 @@ onmessage = function(e) {
       }
     }
 
-    console.log("tests complete", codeResponse);
     if (codeResponse.tests[codeResponse.tests.length-1].valid === true) {
       codeResponse.passed = true;
     }
@@ -40,10 +33,8 @@ onmessage = function(e) {
   //this instantiates all JS written by the user. If any of it errors, it will be caught by the on error event in the controller.
   eval(e.data.code); 
 
-  console.log("inputs", e.data.inputs)
   //if the eval is successful the code will continue.
   var codeResponse = codeProcess(e.data.functionName, e.data.inputs, e.data.answers);
 
-  console.log("end of code response");
   postMessage(codeResponse);
 }
